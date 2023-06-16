4de0cb78e9c0c1b8443386152f2fb0af155d82b1
Daniel Gazcon <daniel.gazcon27@gmail.com>
2023-06-16 16:05:33 -0400

    update model article

--- models/Articles/articlesRepository.js
+++ models/Articles/articlesRepository.js
@@ -9,8 +9,7 @@ const ArticleRepository = Mongoose.model('article', Mongoose.Schema({
     updatedAt: { type: Date, default: new Date(), required: true },
     quantityAvailable: { type: Number, default: 0, required: true },
     unitPrice: { type: Number, default: 0, required: true },
-    packageUnit: { type: Number, default: 0, required: true },
-    totalCost: { type: Number, default: 0, required: true },
+    totalCostPackage: { type: Number, default: 0, required: true },
     profit: { type: Number, default: 0, required: true }
   }))
   
