491f0c349ce280976f28addd1e39e3047e0983cb
Daniel Gazcon <daniel.gazcon27@gmail.com>
2023-08-15 16:19:37 -0400

    modificacion para aceptar peticiones cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const { getConnection } = require("./utils/dbConnection")
 
 const app = require("./index")
+const axios = require("axios");
+const path = require("path")
+require('dotenv').config();
 
 const port = process.env.PORT || 8989;
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var cors = require('cors')
 const {
   errorLogger,
   errorResponder 
@@ -9,6 +10,7 @@ const { routerArticle } = require("./router/articles")
 const { routerPurchase } = require("./router/purchase")
 
 const app = express()
+app.use(cors())
 app.use(express.json())   
 app.use("/api/v1/customer", routerCustomer);
 app.use("/api/v1/article", routerArticle);
